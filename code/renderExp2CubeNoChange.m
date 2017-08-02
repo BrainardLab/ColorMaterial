8b9a35b45176e4c32e9fe81142104d09f8044dc7
aradonjic <radonjic@sas.upenn.edu>
2017-08-02 15:00:16 -0400

    fixed, working

--- code/renderExp2CubeNoChange.m
+++ code/renderExp2CubeNoChange.m
@@ -7,7 +7,7 @@ setpref('RenderToolbox3', 'workingFolder', '/Users1/Shared/Matlab/Experiments/Bl
 
 % renderDir
 currentDir = pwd;
-renderDir = '/Users/Shared/Matlab/Experiments/ColorMaterial/code/renderMaterials';
+renderDir = '/Users/radonjic/Documents/MATLAB/projects/Experiments/ColorMaterial/code';
 addpath(genpath(renderDir));
 
 % use this scene and condition file.
@@ -21,25 +21,31 @@ hints.renderer = 'Mitsuba';
 hints.recipeName = ['ColorSetBlobbyExp2-' date];
 ChangeToWorkingFolder(hints);
 
-isScale = 1;
+isScale = true;
 toneMapFactor = 10;
 nConditions = 1;
 names = {'imageName', 'illuminant', 'reflectance', 'alpha'};
 
-% change alpha levels relative to before. 
+% change alpha levels relative to before.
 alphaLevels = [0.0350    0.0550    0.0800    0.1000  0.1400    0.1700    0.2000];
-% alphaLevels = linspace(0.0200, 0.2000, 7);
 reflectance = [1 2 3 4 5 6 7];
-%mCode = 4;
-cCode = 4; 
+
+stimList =[ ];
+for i = 1:length(reflectance)
+    for j = 1:length(alphaLevels)
+        stimList = [stimList; i,j;];
+    end
+end
+
 for c = 1:nConditions
     if c == 1
         values{2} = 'CM6700.spd';
         condCode = 'Exp2NC';
-        nTests = 7;
     end
-    for t = 1:nTests
-        mCode = t;
+    
+    for t = 1:length(stimList)
+        cCode = stimList(t,1);
+        mCode = stimList(t,2);
         values{1} = [condCode 'C' num2str(cCode), 'M' num2str(mCode)];
         values{3} = [condCode 'CompetitorBlobRef' num2str(cCode) '.spd'];
         values{4} = alphaLevels(mCode);
@@ -48,13 +54,16 @@ for c = 1:nConditions
         
         nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
         radianceDataFiles = BatchRender(nativeSceneFiles, hints);
-                montageName = [values{1} ];
-                montageFile = [values{1} '.png'];
-                % condense multi-spectral renderings into one sRGB montage
-                [SRGBMontage, XYZMontage] = ...
-                    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
-                % display the sRGB montage
-                ShowXYZAndSRGB([], SRGBMontage, montageName);
+        % condense multi-spectral renderings into one sRGB montage
+        if t == 1
+            montageName = [values{1} ];
+            montageFile = [values{1} '.png'];
+            
+            [SRGBMontage, XYZMontage] = ...
+                MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
+            % display the sRGB montage
+            ShowXYZAndSRGB([], SRGBMontage, montageName);
+        end
     end
     emailToStr = 'radonjic@sas.upenn.edu';
     setpref('Internet', 'SMTP_Server', 'smtp-relay.upenn.edu');
@@ -62,34 +71,6 @@ for c = 1:nConditions
     sendmail(emailToStr, 'RenderDone', 'Finished this condition.');
 end
 
-mCode = 4;
-for c = 1:nConditions
-    if c == 1
-        values{2} = 'CM6700.spd';
-        condCode = 'Exp2NC';
-        nTests = 7;
-    end
-    for t = 1:nTests
-        cCode = t;
-        values{1} = [condCode 'C' num2str(cCode), 'M' num2str(mCode)];
-        values{3} = [condCode 'CompetitorBlobRef' num2str(cCode) '.spd'];
-        values{4} = alphaLevels(mCode);
-        conditionsFile = [values{1} '.txt'];
-        conditionsFile = WriteConditionsFile(conditionsFile, names, values);
-        
-        nativeSceneFiles = MakeSceneFiles(parentSceneFile, conditionsFile, mappingsFile, hints);
-        radianceDataFiles = BatchRender(nativeSceneFiles, hints);
-                montageName = [values{1} ];
-                montageFile = [values{1} '.png'];
-                % condense multi-spectral renderings into one sRGB montage
-                [SRGBMontage, XYZMontage] = ...
-                    MakeMontage(radianceDataFiles, montageFile, toneMapFactor, isScale, hints);
-                % display the sRGB montage
-                ShowXYZAndSRGB([], SRGBMontage, montageName);
-    end
-    emailToStr = 'radonjic@sas.upenn.edu';
-    setpref('Internet', 'SMTP_Server', 'smtp-relay.upenn.edu');
-    setpref('Internet', 'E_Mail', emailToStr);
-    sendmail(emailToStr, 'RenderDone', 'Finished this condition.');
-end
+
+
    
