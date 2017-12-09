57f2f707e7133d6e472bcd9c82790e0207a684c3
Nicolas Cottaris <cottaris@sas.upenn.edu>
2017-12-08 19:08:52 -0500

    working on seting up the new experiment

---
+++ code/convertMSImageToRGBColorMaterialExp3.m
@@ -0,0 +1,66 @@
+% ConvertMSImageToRGBColorMaterialExp2
+% Converts out hyperspectral stimulus images to RGB for display
+
+% 02/11/15 ar Wrote it.
+% 12/08/17 ar Adapted it for Experiment 2. 
+
+% Initialize
+clear all; close;
+
+% Define directories
+multispectralDataFolder = '/Users1/Shared/Matlab/Experiments/Blobby/ColorSetBlobbyExp2-03-Aug-2017/renderings/Mitsuba';
+LMSdataFolder = '/Users1/Shared/Matlab/Experiments/Blobby/ColorSetBlobbyExp2-03-Aug-2017/LMS';
+RGBdataFolder = '/Users1/Shared/Matlab/Experiments/Blobby/ColorSetBlobbyExp2-03-Aug-2017/RGB';
+
+% Load color matching functions, prepare calibration file.
+load T_cones_ss2
+S = [400, 10, 31];
+cal = LoadCalFile('EyeTrackerLCD');
+cal = SetGammaMethod(cal, 0);
+calLMS = SetSensorColorSpace(cal, T_cones_ss2,  S_cones_ss2);
+
+% Define parameters
+load('Exp2ImageList.mat')
+scaleTo = 1;%
+
+maxPrimary = 1.67*scaleTo; % this is the one for the new set of stimuli. 
+% Create image lists with respect to parameters defined above .
+
+% Convert the images from MS to LMS to rgb.
+% Because these are not rendered relative to the monitor calibration, we
+% have to scale them to fit the gamut.
+tic
+for i = 1:length(imageList.imageName)
+    % Pick and image and its new name
+    cd(multispectralDataFolder);
+    thisImageName = imageList.imageName{i};
+    image = load([imageList.imageName{i} , '.mat']);
+    
+    % Convert the image to LMS and save it.
+    sensorImageLMS = MultispectralToSensorImage(image.multispectralImage, image.S, T_cones_ss2, S_cones_ss2);
+    cd(LMSdataFolder);
+    save([thisImageName, '-LMS.mat'], 'sensorImageLMS')
+    
+    % Convert it from LMS to rgb
+    [temp,m,n] = ImageToCalFormat(sensorImageLMS);
+    uncorrectedRGB = SensorToPrimary(calLMS, temp); % get gamma-uncorrected settings
+    thisMax(i) = max(uncorrectedRGB(:));
+    uncorrectedRGB = uncorrectedRGB./maxPrimary;
+    uncorrectedRGB = uncorrectedRGB.*scaleTo;
+    % check for pixels that are out of gamut (should not be any on the
+    % high-end. 
+    if sum(uncorrectedRGB(uncorrectedRGB>1))>1
+         error('Settings out of range.');
+    end
+    if (sum(uncorrectedRGB(uncorrectedRGB<0)))
+        disp(sum(uncorrectedRGB(uncorrectedRGB<0)))
+    end
+    % Convert the scaled primaries to RGB and save. 
+    [gamut,~] = PrimaryToGamut(calLMS,uncorrectedRGB);
+    settings = GamutToSettings(calLMS,gamut);
+    sensorImageRGB = CalFormatToImage(settings, m, n);
+    cd(RGBdataFolder);
+    save([thisImageName, '-RGB.mat'], 'sensorImageRGB')
+    clear sensorImageLMS imageName image temp tempRGB uncorrectedRGB sensorImageRGB settings gamut
+end
+toc
