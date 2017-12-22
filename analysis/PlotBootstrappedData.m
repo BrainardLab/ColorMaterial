87f6b7fdda4c74d14d917e69a80567dc5a8521bc
Ana Radonjic <radonjic@sas.upenn.edu>
2017-12-22 14:00:25 -0500

    plotting the solutions

--- analysis/PlotBootstrappedData.m
+++ analysis/PlotBootstrappedData.m
@@ -16,7 +16,7 @@ whichExperiment = 'Pilot';
 % Set paramters for a given expeirment.
 switch whichExperiment
     case 'Pilot'
-        figAndDataDir = [analysisDir '/' whichExperiment  '/' ];
+        figAndDataDir = [analysisDir '/' whichExperiment   ];
         subjectList = {'zhr','mcv', 'flj', 'vtr','scd'};
         conditionCode = {'NC'};
         nBlocks = 25;
@@ -32,7 +32,7 @@ nConditions = length(conditionCode);
 nModelTypes = 1; 
 showOutcome = 1; 
 nSubjects = length(subjectList); 
-CIrange = 68.27; % confidence interval range. 
+CIrange = 95; % confidence interval range. 
 CIlo = (1-CIrange/100)/2;
 CIhi = 1-CIlo;
 
@@ -41,7 +41,7 @@ CIhi = 1-CIlo;
 % We should not set it again here. Commenting it out. 
 % params.addNoise = true; 
 %params.maxPositionValue = max(params.F.GridVectors{1});
-nRepetitions = 400; 
+nRepetitions = 150; 
 %% Run the bootstrapping for each subject and condition 
 % Leaving an option to enable different models (although for now we just have one model). 
 % To introduce other models, we can redefine some of the parameters here. 
@@ -49,111 +49,46 @@ for s = 1:length(subjectList)
     switch whichExperiment
         case 'Pilot'
             clear thisSubject
-        %    load([figAndDataDir '/' subjectList{s} 'SolutionBootstrap' num2str(nRepetitions) '-weightVary-smoothSpacing.mat'])
-            load([figAndDataDir '/' subjectList{s} 'NCweightVary-Bootstrap.mat'])
-        
+           % k = load([figAndDataDir '/' subjectList{s} 'SolutionBootstrap' num2str(nRepetitions) '-weightVary-smoothSpacing.mat']);
+            load([figAndDataDir '/' subjectList{s} 'Bootstrap' num2str(nRepetitions) '-SmoothSpacingQuad.mat'])
+            %      load([figAndDataDir '/' subjectList{s} 'SolutionBootstrap' num2str(nRepetitions) '-weightVary-smoothSpacing.mat'])
+            
         case 'E1P2FULL'
             clear thisSubject
             load([figAndDataDir '/' subjectList{s} 'SummarizedDataFULL.mat']); % data
             
     end
     subject{s} = thisSubject; clear thisSubject; 
-%     for whichCondition = 1:length(conditionCode)
-%         for jj = 1:size(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams,2)
-%             
-%             subject{s}.condition{whichCondition}.bootstrapMeans(jj) = ...
-%                 mean(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,jj));
-%             
-%             subject{s}.condition{whichCondition}.bootstrapCI(jj,1) = ...
-%                 prctile(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,jj),...
-%                 100*CIlo);
-%             
-%             subject{s}.condition{whichCondition}.bootstrapErrorbarLo(jj) = ...
-%                 subject{s}.condition{whichCondition}.bootstrapMeans(jj)...
-%                 - subject{s}.condition{whichCondition}.bootstrapCI(jj,1);
-%             
-%             subject{s}.condition{whichCondition}.bootstrapCI(jj,2) = ...
-%                 prctile(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,jj),...
-%                 100*CIhi);
-%              
-%             subject{s}.condition{whichCondition}.bootstrapErrorbarHi(jj) = ...
-%                 subject{s}.condition{whichCondition}.bootstrapCI(jj,2) - ...
-%                 subject{s}.condition{whichCondition}.bootstrapMeans(jj);
-%         end
-%     end
+
  end
 
-% % Plot
-% figure; clf; hold on;
-% for i = 1:3
-%     subplot(1,3,i); hold on; 
-%     for s  = 1:length(subjectList)
-%         errorbar(s, subject{s}.condition{whichCondition}.bootstrapMeans(i), ...
-%             subject{s}.condition{whichCondition}.bootstrapErrorbarLo(i),...
-%             subject{s}.condition{whichCondition}.bootstrapErrorbarHi(i),...
-%             'ro');
-%         if i == 3
-%              axis([0 6 0 1])
-%         else
-%              axis([0 6 1 8])
-%         end
-%         axis square
-%     end
-% end
-% 
-% for s = 1:length(subjectList)
-%     figure; 
-%     subplot(1,3,1);
-%     plot(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,1), ...
-%         subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,2), 'ro');
-%     axis([0 9 0 9])
-%     xlabel('color spacing'); 
-%     ylabel('material spacing'); 
-%     axis square
-%     
-%     subplot(1,3,2);
-%     plot(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,2), ...
-%         subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,3), 'ro');
-%     axis([0 9 0 1])
-%     xlabel('material spacing'); 
-%     ylabel('weight');
-%     axis square
-%     
-%     subplot(1,3,3);
-%     plot(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,1), ...
-%         subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,3), 'ro');
-%     axis([0 9 0 1])
-%     xlabel('color spacing');
-%     ylabel('weight');
-%     axis square
-% end
-% 
-% 
-% % How well does the mean fit fit the data
-% weibullplots = 0 ;
-% saveFigs  = 0; 
-
-% % Compute probabilities based on the mean solution. 
-% pairInfo = load([figAndDataDir 'pairIndicesPilot.mat']); 
-% nTrials = ones(size(subject{s}.condition{whichCondition}.pFirstChosen)); 
 
 % make a plot for all subjects
 color = [0 153 153]'./255;
 thisMarkerSize = 16;
 figure;  clf; hold on;
-whichCondition = 1; 
+whichCondition = 1;
 xTick = 0:(length(subjectList));
-    labelList{length(subjectList)+2} = '';
-    for i = 1:length(subjectList)
-        labelList{i+1} = subjectList{i};
-    end
+labelList{length(subjectList)+2} = '';
+for i = 1:length(subjectList)
+    labelList{i+1} = subjectList{i};
+end
 for s = 1:nSubjects
-  %  plot(s, subject{s}.condition{whichCondition}.bootstrapMeans(3), 'o' , 'MarkerFaceColor', color , 'MarkerEdgeColor', color, 'MarkerSize', thisMarkerSize)
-    errorbar(s, mean(subject{s}.condition{whichCondition}.bootstrap.returnedParamsTraining(:,end-1)),...
-        std(subject{s}.condition{whichCondition}.bootstrap.returnedParamsTraining(:,end-1)), ...
-        std(subject{s}.condition{whichCondition}.bootstrap.returnedParamsTraining(:,end-1)), ...
+    CI1(s) = ...
+        prctile(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,end-1),...
+        100*CIlo);
+    
+    CI2(s) = ...
+        prctile(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,end-1),...
+        100*CIhi);
+    meanW(s) = mean(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,end-1)); 
+    stdW(s) = std(subject{s}.condition{whichCondition}.bootstrapStructure.returnedParams(:,end-1)); 
+    errorbar(s,  meanW(s),  meanW(s)-CI1(s),  CI2(s)-meanW(s), ...
         'o' , 'MarkerFaceColor', color , 'MarkerEdgeColor', color, 'MarkerSize', thisMarkerSize, ...
         'color', color, 'LineWidth', 1.2)
+%     errorbar(s, meanW(s),stdW(s), stdW(s), ...
+%         'o' , 'MarkerFaceColor', color , 'MarkerEdgeColor', color, 'MarkerSize', thisMarkerSize, ...
+%         'color', color, 'LineWidth', 1.2)
 end
 axis([0 nSubjects+1 0 1])
 xlabel('Observers','FontName','Helvetica','FontSize',20);
@@ -163,7 +98,7 @@ set(gca,'YTickLabel',num2str(get(gca,'YTick')','%.2f'))
 set(gca, 'Xtick', xTick,'FontName','Helvetica','FontSize',20);
 set(gca, 'XTickLabel', labelList, 'FontName','Helvetica','FontSize',20);
 cd ..
-FigureSave(['PilotResultsAll'],gcf,'pdf');
+FigureSave(['PilotResultsAllLinearModel'],gcf,'pdf');
 
 % Save in the right folder.
 cd(figAndDataDir)
--- analysis/PlotColorMaterialModelSolutions.m
+++ analysis/PlotColorMaterialModelSolutions.m
@@ -9,14 +9,14 @@ switch whichExperiment
     case 'E1P2'
         % Specify other experimental parameters
         subjectList = { 'mdc', 'nsk'};
-        %         subjectList = {'ifj', 'ueh', 'krz', 'mdc', 'nsk', 'zpf'};
+        %  subjectList = {'ifj', 'ueh', 'krz', 'mdc', 'nsk', 'zpf'};
         conditionCode = {'NC', 'CY', 'CB'};
         
         figAndDataDir = ['/Users/ana/Dropbox (Aguirre-Brainard Lab)/CNST_analysis/ColorMaterial/Experiment1'];
         load([figAndDataDir '/' 'ParamsE1P2FULL.mat'])
     case 'Pilot'
         % Specify other experimental parameters
-        subjectList = {'mcv'}; % 'vtr', 'scd', 'mcv', 'flj'};
+        subjectList = {'zhr', 'vtr', 'scd', 'mcv', 'flj'};
         conditionCode = {'NC'};
         figAndDataDir = ['/Users/ana/Dropbox (Aguirre-Brainard Lab)/CNST_analysis/ColorMaterial/Pilot'];
         load([figAndDataDir '/' 'pairIndicesPilot.mat'])
@@ -24,53 +24,54 @@ end
 
 nSubjects = length(subjectList);
 nConditions = length(conditionCode);
-saveFig = 0; 
-weibullplots = 0; 
-        
-for s = 1:length(subjectList)
-    close all;
-    for whichCondition = 1:nConditions
-        interpCode = 'C';
-        if strcmp(whichExperiment, 'E1P2')
-            load([figAndDataDir '/' interpCode  subjectList{s} 'SolutionNew-weightVary.mat'])
-        else
-            load([figAndDataDir '/'   subjectList{s} 'SolutionNew1-weightVary-smoothSpacing.mat'])
-        end
-        
-        ColorMaterialModelPlotSolution(thisSubject.condition{whichCondition}.pFirstChosen, ...
-            thisSubject.condition{whichCondition}.bootstrapStructure.predictedProbabilitiesBasedOnSolution, ...
-            thisSubject.condition{whichCondition}.bootstrapStructure.returnedParams,...
-            indexMatrix, params, figAndDataDir, saveFig, weibullplots)
-               
-%         ColorMaterialModelPlotSolution(dataSet{whichSet}.probabilitiesFromSimulatedData, ...
-%             dataSet{whichSet}.predictedProbabilitiesBasedOnSolution, ...
-%             dataSet{whichSet}.returnedParams, ...
-%             indexMatrix, params, pwd,  saveFig, weibullplots);
-    end
+saveFig = 0;
+weibullplots = 0;
+params = getqPlusPilotExpParams;
+params = getqPlusPilotModelingParams(params);
+% What sort of position fitting ('full', 'smoothSpacing').
+params.whichPositions = 'smoothSpacing';
+if strcmp(params.whichPositions, 'smoothSpacing')
+    params.smoothOrder = 1;
 end
 
-%
-thisSlope = paramsRange{1}(end-1,:); 
-[~, sortIndex] = sort(thisSlope);
-nSubjects= (length(subjectList));
-xTick = 0:(length(subjectList));
-thisMarkerSize = 18;
-labelList{length(subjectList)+2} = '';
-for i = 1:length(subjectList)
-    labelList{i+1} = subjectList{sortIndex(i)};
+% Does material/color weight vary in fit? ('weightVary', 'weightFixed'). 
+params.whichWeight = 'weightVary';
+
+for s = 1:length(subjectList)
+    params.subjectName = subjectList{s};
+    close all;
+    for whichCondition = 1:nConditions
+        load([figAndDataDir '/'   subjectList{s} 'Solution-smoothSpacing' num2str(params.smoothOrder) '.mat'])
+        
+        ColorMaterialModelPlotSolution(thisSubject.condition{whichCondition}.pFirstChosen, ...
+            thisSubject.condition{whichCondition}.predictedProbabilitiesBasedOnSolution, ...
+            thisSubject.condition{whichCondition}.returnedParams,...
+            indexMatrix, params, figAndDataDir, saveFig, weibullplots)
+        
+    end
+    FigureSave([subjectList{s} num2str(params.smoothOrder) ],gcf,'pdf');
 end
-color = [0 153 153]'./255;
-thisMarkerSize = 16;
-figure;  clf; hold on;
-for s = 1:(length(subjectList))
-    plot(s, thisSlope(sortIndex(s)), 'o' , 'MarkerFaceColor', color , 'MarkerEdgeColor', color, 'MarkerSize', thisMarkerSize)
-end
-axis([0 nSubjects+1 0 1])
-xlabel('Subjects','FontName','Helvetica','FontSize',20);
-ylabel('Color Weigth','FontName','Helvetica','FontSize',20);%set(gca, 'Ytick', [1:nLevels]);
-set(gca, 'YTick', [0.0:0.5:2]);
-set(gca,'YTickLabel',num2str(get(gca,'YTick')','%.1f'))
-set(gca, 'Xtick', xTick,'FontName','Helvetica','FontSize',20);
-set(gca, 'XTickLabel', labelList, 'FontName','Helvetica','FontSize',20);
-cd ..
-FigureSave(['DavidSlideB'],gcf,'pdf');
+
+% %
+% thisSlope = paramsRange{1}(end-1,:); 
+% [~, sortIndex] = sort(thisSlope);
+% nSubjects= (length(subjectList));
+% xTick = 0:(length(subjectList));
+% thisMarkerSize = 18;
+% labelList{length(subjectList)+2} = '';
+% for i = 1:length(subjectList)
+%     labelList{i+1} = subjectList{sortIndex(i)};
+% end
+% color = [0 153 153]'./255;
+% thisMarkerSize = 16;
+% figure;  clf; hold on;
+% for s = 1:(length(subjectList))
+%     plot(s, thisSlope(sortIndex(s)), 'o' , 'MarkerFaceColor', color , 'MarkerEdgeColor', color, 'MarkerSize', thisMarkerSize)
+% end
+% axis([0 nSubjects+1 0 1])
+% xlabel('Subjects','FontName','Helvetica','FontSize',20);
+% ylabel('Color Weigth','FontName','Helvetica','FontSize',20);%set(gca, 'Ytick', [1:nLevels]);
+% set(gca, 'YTick', [0.0:0.5:2]);
+% set(gca,'YTickLabel',num2str(get(gca,'YTick')','%.1f'))
+% set(gca, 'Xtick', xTick,'FontName','Helvetica','FontSize',20);
+% set(gca, 'XTickLabel', labelList, 'FontName','Helvetica','FontSize',20);
