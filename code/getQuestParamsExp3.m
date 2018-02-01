d0d726b4a2e7d356acd1f4bb5c7462a798230ad5
Ana Radonjic <radonjic@sas.upenn.edu>
2018-02-01 09:15:31 -0800

    fixes for the cubic paramters

--- code/getQuestParamsExp3.m
+++ code/getQuestParamsExp3.m
@@ -5,10 +5,27 @@ function params = getQuestParamsExp3
 % 09/12/2017 ar Pulled this from the demo code. 
 
 % Range over which quest+ will look for the best position of the params. 
-params.upperLin = 6;
-params.upperQuad = 0.5;
-params.upperCubic = 0.5;
-params.weights = linspace(0.05,0.95,5);
+
+lowerLin = 1;
+upperLin = 6;
+nLin = 4;
+
+lowerQuad = -0.3;
+upperQuad = -lowerQuad;
+nQuad = 3;
+
+lowerCubic = -0.3;
+upperCubic = -lowerCubic;
+nCubic = 3;
+
+lowerWeight = 0.05;
+upperWeight = 0.95;
+nWeight = 5;
+
+params.weights = linspace(lowerWeight,upperWeight,nWeight);
+params.Lin = linspace(lowerLin,upperLin,nLin);
+params.Quad = linspace(lowerQuad,upperQuad,nQuad);
+params.Cubic = linspace(lowerCubic,upperCubic,nCubic);
 
 % Details of the experimental design
 params.stimUpperEnds = [1 2 3];
@@ -16,5 +33,5 @@ params.nTrialsPerQuest = 30;
 params.questOrderIn = [0 1 2 3 3 3 3 3 3];
 
 % initialize or not
-params.DO_INITIALIZE = false; 
+params.DO_INITIALIZE = true; 
 params.initDir = fullfile([getpref('ColorMaterial', 'dataFolder'), '/E3']);
