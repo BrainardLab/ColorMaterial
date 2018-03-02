0053b186dd9c8a799caa6094d092ebdaf4a26c66
aradonjic <radonjic@sas.upenn.edu>
2018-03-02 12:52:45 -0500

    change to upper case (interpCode)

--- analysis/getqPlusPilotModelingParams.m
+++ analysis/getqPlusPilotModelingParams.m
@@ -12,6 +12,6 @@ params.tryWeightValues = [0.5 0.2 0.8];
 params.whichMethod = 'lookup'; % could be also 'simulate' or 'analytic'
 params.nSimulate = 1000; % for method 'simulate'
 load colorMaterialInterpolateFunCubiceuclidean.mat
-params.interpCode = 'cubic';
+params.interpCode = 'Cubic';
 params.F = colorMaterialInterpolatorFunction; % for lookup.
 
