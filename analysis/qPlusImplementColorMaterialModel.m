c952caa5e59392792c92ffeb4006fa4f3b44cb41
aradonjic <radonjic@sas.upenn.edu>
2018-04-05 16:11:26 -0400

    modifying the code for analysis

--- analysis/qPlusImplementColorMaterialModel.m
+++ analysis/qPlusImplementColorMaterialModel.m
@@ -7,6 +7,7 @@ clear; close
 whichExperiment = 'E3';
 analysisDir = fullfile(getpref('ColorMaterial', 'analysisDir'), 'E3');
 codeDir  = fullfile(getpref('ColorMaterial', 'mainExpDir'), 'analysis'); 
+dataDir = getpref('ColorMaterial', 'dataFolder');
 
 % Exp parameters
 % Specify other experimental parameters
@@ -24,6 +25,11 @@ params.interpCode = 'Cubic';
 
 % Set up initial modeling paramters (add on)
 params = getqPlusPilotModelingParams(params);
+tempParams = params; 
+tempParams.whichPositions = 'smoothSpacing'; 
+tempParams.smoothOrder = 3; 
+
+nBlocks = 8;
 
 % Set up more modeling parameters
 % What sort of position fitting ('full', 'smoothOrder').
@@ -34,15 +40,44 @@ if strcmp(params.whichPositions, 'smoothSpacing')
 end
 % Does material/color weight vary in fit? ('weightVary', 'weightFixed').
 params.whichWeight = 'weightVary';
+indices.stimPairs = 1:4;
+indices.response1 = 5;
+indices.nTrials = 6;
+whichExperiment = 'E3'
 
 % For each subject and each condition, run the model and basic plots
 for s = 1:nSubjects
-    
-    load([analysisDir '/' subjectList{s} 'SummarizedqPlusData.mat']);
-    subject{s} = thisSubject; clear thisSubject; 
-    
-    % Implement the model, for each condition. 
     for c = 1:nConditions
+        subject{s}.condition{c}.trialData = [];
+        for b  = 1:nBlocks(s)
+            warnState = warning('off','MATLAB:dispatcher:UnresolvedFunctionHandle');
+            tempSubj = load([dataDir '/' whichExperiment '/' subjectList{s} '/' subjectList{s}  '-'  whichExperiment  '-' num2str(b) '.mat']);
+            warning(warnState);
+            for t = 1:length(tempSubj.params.data.trialData)
+                subject{s}.condition{c}.trialData = [subject{s}.condition{c}.trialData; ...
+                    tempSubj.params.data.trialData(t).stim, tempSubj.params.data.trialData(t).outcome];
+            end
+            clear tempSubj
+        end
+        
+        
+        % Concatenate across blocks
+        subject{s}.condition{c}.rawTrialData = subject{s}.condition{c}.trialData;
+        subject{s}.condition{c}.newTrialData = qPlusConcatenateRawData(subject{s}.condition{c}.rawTrialData, indices);
+        
+        % Convert the information about pairs to 'our prefered representation'
+        subject{s}.condition{c}.pairColorMatchColorCoords = subject{s}.condition{c}.newTrialData(:,1);
+        subject{s}.condition{c}.pairMaterialMatchColorCoords = subject{s}.condition{c}.newTrialData(:,2);
+        subject{s}.condition{c}.pairColorMatchMaterialCoords = subject{s}.condition{c}.newTrialData(:,3);
+        subject{s}.condition{c}.pairMaterialMatchMaterialCoords = subject{s}.condition{c}.newTrialData(:,4);
+        subject{s}.condition{c}.firstChosen = subject{s}.condition{c}.newTrialData(:,5);
+        subject{s}.condition{c}.newNTrials = subject{s}.condition{c}.newTrialData(:,6);
+        subject{s}.condition{c}.pFirstChosen = subject{s}.condition{c}.firstChosen./subject{s}.condition{c}.newNTrials;
+        
+    % Implement the model, for each condition. 
+    params.qpParamsStart = false; 
+            params.qpInitialParams = thisSet.initialParams;
+           
         [subject{s}.condition{c}.returnedParams, subject{s}.condition{c}.logLikelyFit, ...
             subject{s}.condition{c}.predictedProbabilitiesBasedOnSolution] = ...
             FitColorMaterialModelMLDS(subject{s}.condition{c}.pairColorMatchColorCoords, ...
@@ -51,6 +86,7 @@ for s = 1:nSubjects
             subject{s}.condition{c}.pairMaterialMatchMaterialCoords,...
             subject{s}.condition{c}.firstChosen , subject{s}.condition{c}.newNTrials,...
             params);
+        
         [subject{s}.condition{c}.returnedMaterialMatchColorCoords, ...
             subject{s}.condition{c}.returnedColorMatchMaterialCoords, ...
             subject{s}.condition{c}.returnedW,...
