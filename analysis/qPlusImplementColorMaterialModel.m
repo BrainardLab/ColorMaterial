97264a1b21bd4a1390b3a2cbc10954173abf02bc
David Brainard <brainard@psych.upenn.edu>
2018-04-10 14:38:45 -0400

    Reviewed and commented.

--- analysis/qPlusImplementColorMaterialModel.m
+++ analysis/qPlusImplementColorMaterialModel.m
@@ -11,20 +11,30 @@ dataDir = fullfile(getpref('ColorMaterial', 'dataFolder'),'/E3/');
 codeDir  = fullfile(getpref('ColorMaterial', 'mainExpDir'), 'analysis');
 
 % Specify other experimental parameters
-nSets = 8;
+nBlocks = 8;
 distances = {'euclidean'};
 
 % Load structure that matches the experimental design of
 % our initial experiments.
+%
+% This is stuff like the number of competitors and thier
+% nominal positions -- things that are fixed throughout
+% a particular experimental subproject.
 params = getqPlusPilotExpParams;
 
 params.whichDistance = 'euclidean';
 params.interpCode = 'Cubic';
 
-% Set up initial modeling paramters
+% Add to the parameters structure parameters that 
+% define the modeling we are doing.
+%
+% This is things like grid search parameters and information
+% that defines how we compute likihood in the fitting.
 params = getqPlusPilotModelingParams(params);
 
-% Use this adapted structure to get qpParams for likelihood estimation. 
+% Tweak structure so that we can use it with Quest+ routines.
+% This allows us to use Quest+ machinary to get some initial parameters
+% for our search. Quest+ can't handle the full model.
 tempParams = params; 
 tempParams.whichPositions = 'smoothSpacing'; 
 tempParams.smoothOrder = 3; 
@@ -40,13 +50,13 @@ params.whichWeight = 'weightVary';
 %  If false, we use our rich set of 75 diffent points (takes much longer)  
 params.qpParamsStart = false; 
 
-% Set Indices for concatinating trial data
+% Set indices for concatinating trial data 
 indices.stimPairs = 1:4;
 indices.response1 = 5;
 indices.nTrials = 6;
 
-% load the same qPlus params as in the experiment. 
-% tempqPParams = run([getpref('ColorMaterial', 'mainCodeDir') '/getQuestParamsExp3']);
+% Load the same qPlus params as in the experiment. 
+% eval(['tempqPParams = fullfile(getpref(''ColorMaterial'',''mainCodeDir''),''getQuestParamsExp3'');']);
 lowerLin = 0.5;
 upperLin = 6;
 nLin = 5;
@@ -63,16 +73,18 @@ lowerWeight = 0.05;
 upperWeight = 0.95;
 nWeight = 5;
 
+% Subjects to analyze
 subjectList = {'test'}; 
 
 % Load the initialization file. 
 warnState = warning('off','MATLAB:dispatcher:UnresolvedFunctionHandle');
 qpInit = load([dataDir, 'initalizedQuestsExp3-09-Apr-2018.mat']);
 warning(warnState);
-for ss = 1:length(subjectList) % we can modigy this is we have sets with different position spacings.
+for ss = 1:length(subjectList) 
     for d = 1:length(distances)
             
         % Set up the quest data structure that updates. 
+        clear questDataAllTrials;
         questDataAllTrials = qpInit.questData{end};
         questDataAllTrials.noentropy = true; 
         
@@ -80,13 +92,14 @@ for ss = 1:length(subjectList) % we can modigy this is we have sets with differe
         n = 0;
         
         % Load and reformat all trials. 
-        for i = 1:nSets
+        for i = 1:nBlocks
             % Load the data set
             fileName = [subjectList{ss}, '/' subjectList{ss}, '-E3-' num2str(i) '.mat'];
-            warning(warnState);
             clear thisTempSet
+            warnState = warning('off','MATLAB:dispatcher:UnresolvedFunctionHandle');
             thisTempSet = load([dataDir, fileName]);
-            
+            warning(warnState);
+
             % Get few params that do not change across trials/blocks and that we
             % need to compute qPlus likelihoods. 
             qpPF = thisTempSet.params.data.qpPF;
@@ -94,7 +107,9 @@ for ss = 1:length(subjectList) % we can modigy this is we have sets with differe
             
             for t = 1:length(thisTempSet.params.data.trialData)
                 n = n+1;
-                % Reformat the data for stimCounts below. 
+                % Reformat the data for stimCounts below. Note that we need
+                % the (n,1) format so that the struct array will be a
+                % column thing, which is what Quest+ needs.
                 thisSubject.trialData(n,1).stim = thisTempSet.params.data.trialData(t).stim;
                 thisSubject.trialData(n,1).outcome = thisTempSet.params.data.trialData(t).outcome;
                 
@@ -108,11 +123,16 @@ for ss = 1:length(subjectList) % we can modigy this is we have sets with differe
             end
         end
         
-        % Compute qPlus outcomes and some diagnostics
-        clear psiParamsIndex psiParamsQuest psiParamsFit
+        % Compute qPlus estimates and some diagnostics
+        clear psiParamsIndex psiParamsQuest psiParamsFit stimCounts
+        
+        % Get discretized MAP estimate from the combined quest structure we
+        % built above.
         stimCounts = qpCounts(qpData(thisSubject.trialData),nOutcomes);
         psiParamsIndex = qpListMaxArg(questDataAllTrials.posterior);
         psiParamsQuest = thisTempSet.params.data.psiParamsDomain(psiParamsIndex,:);
+        
+        % Do max likelihood fit using the MAP estiamte to start
         psiParamsFit = qpFit(thisSubject.trialData,qpPF,psiParamsQuest(:),nOutcomes,...
             'lowerBounds', [1/upperLin -upperQuad -upperCubic ...
             1/upperLin -upperQuad -upperCubic 0], ...
@@ -131,7 +151,14 @@ for ss = 1:length(subjectList) % we can modigy this is we have sets with differe
         % Concatenate across blocks
         thisSubject.newTrialData = qPlusConcatenateRawData(thisSubject.rawTrialData, indices);
         
-        % Convert the information about pairs to 'our prefered representation'
+        % Convert the information about pairs and outcomes to 'our prefered representation'
+        % This is to pass to our non-Quest+ fitting routine, which can find
+        % high likelihood solutions than the Quest+ version, possibly
+        % because it knows more about stimulus constraints. Also, it can
+        % fit the full model.
+        %
+        % Our routine works in log10 likelihoods, which is why we convert
+        % likelihoods above and not here.
         thisSubject.pairColorMatchColorCoords = thisSubject.newTrialData(:,1);
         thisSubject.pairMaterialMatchColorCoords = thisSubject.newTrialData(:,2);
         thisSubject.pairColorMatchMaterialCoords = thisSubject.newTrialData(:,3);
@@ -150,10 +177,16 @@ for ss = 1:length(subjectList) % we can modigy this is we have sets with differe
         % Extract parameters
         [thisSubject.returnedMaterialMatchColorCoords, thisSubject.returnedColorMatchMaterialCoords, ...
             thisSubject.returnedW, thisSubject.returnedSigma]  = ColorMaterialModelXToParams(thisSubject.returnedParams, params);
-        fprintf('Log likelihood of data given our model parameters: %0.2f\n', thisSubject.logLikelyFit);
+        fprintf('Log 10 likelihood of data given our model parameters: %0.2f\n', thisSubject.logLikelyFit);
+        
+        % Could (and probably should) do our code starting at qp params
+        % here, and take whichever has the best overall likelihood. Ana
+        % wins an ice cream if she implements this and for no cases in our next
+        % experiment do the qp parameters lead to a better solution.  Otherwise David
+        % wins the ice cream.
         
         % Save the outcome
-        cd (getpref('ColorMaterial', 'demoDataDir'))
+        cd (getpref('ColorMaterial', 'anaysisDir'))
         subject{ss,d} = thisSubject; 
         save([subjectList{ss} distances{d} 'Fit'], 'thisSubject'); clear thisSubject
         cd (codeDir)
