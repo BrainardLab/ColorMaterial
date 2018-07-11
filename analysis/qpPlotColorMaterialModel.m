95f0c0571a68b02110e72f5185a78016823dce80
Ana Radonjic <radonjic@sas.upenn.edu>
2018-07-11 18:29:48 -0400

    added option to plot color-material data, extracted from qplus data

--- analysis/qpPlotColorMaterialModel.m
+++ analysis/qpPlotColorMaterialModel.m
@@ -18,8 +18,7 @@ codeDir  = '/Users/ana/Documents/MATLAB/projects/Experiments/ColorMaterial/analy
 % Specify subject model that is being fit.
 subjectList = {'hmneuclideanFull', 'dcacityblockFull', 'gfneuclideanCubic', 'lmacityblockQuadratic', 'ofvcityblockFull',...
     'selcityblockQuadratic', 'ckfeuclideanCubic', 'lzacityblockQuadratic', 'ascityblockCubic', 'jcdcityblockLinear', 'nkheuclideanFull', 'nzfcityblockFull'};
-subjectListID = {'hmn','dca', 'gfn', 'lma', 'ofv', 'sel','ckf', 'lza',  'as', 'jcd',  'nkh', 'nzf'};
-
+subjectListID = {'hmn','dca', 'gfn', 'lma', 'ofv', 'sel','ckf', 'lza',  'as ', 'jcd',  'nkh', 'nzf'};
 % subjectList = {'nkheuclideanFull'};
 % subjectListID = {'nkh'};
  
@@ -33,12 +32,12 @@ for s = 1:nSubjects
     % for ww = 1:9
     clear params
     load([analysisDir '/' subjectList{s}, 'Fit.mat'])
-    
+    cmData{s} = load([analysisDir '/' subjectListID{s}, 'cmData.mat']); 
     % fixed weight option
     %load([analysisDir '/' subjectList{s}, num2str(ww) 'FitFixedWeight.mat'])
     
     subject{s} = thisSubject; clear thisSubject
-    params.subjectName = subjectList{s};
+    params.subjectName = subjectListID{s};
     
     subject{s}.pFirstChosen = ...
         subject{s}.firstChosen./subject{s}.newNTrials;
@@ -47,7 +46,7 @@ for s = 1:nSubjects
     [colorSlope(s), materialSlope(s)] = qPlusColorMaterialModelPlotSolution(subject{s}.pFirstChosen, ...
         subject{s}.predictedProbabilitiesBasedOnSolution, tmpNewTrials,...
         subject{s}.returnedParams,...
-        params, analysisDir, saveFig);
+        params, analysisDir, saveFig, cmData{s}.cmDataPoints);
     weight(s) = subject{s}.returnedW;
 %    ll(s,ww) = subject{s}.logLikelyFit;
     if saveFig
