712300c579ee4f80ac65b58b4d2cefaff0938ac0
Ana Radonjic <radonjic@sas.upenn.edu>
2018-08-23 15:36:29 -0400

    clean up

--- analysis/ExtractingTrialsForProbabilityCalculations.m
+++ analysis/ExtractingTrialsForProbabilityCalculations.m
@@ -1,12 +1,15 @@
 % ExtractingTrailsForProbabiltyCalculations
 % Find trials that have repeated more than once and sort them for plotting.
+% This is used in to plot the data against color material matching
+% functions. 
+% 07/xx/2018 ar Wrote it. 
 
 % Initialize;
 clear; close all;
 
 subjectList = {'hmneuclideanFull', 'dcacityblockFull', 'gfneuclideanCubic', 'lmacityblockQuadratic', 'ofvcityblockFull',...
-    'selcityblockQuadratic', 'ckfeuclideanCubic', 'lzacityblockQuadratic', 'ascityblockCubic', 'jcdcityblockLinear', 'nkheuclideanFull', 'nzfcityblockFull'};
-subjectListID = {'hmn','dca', 'gfn', 'lma', 'ofv', 'sel','ckf', 'lza',  'as ', 'jcd',  'nkh', 'nzf'};
+    'selcityblockQuadratic', 'ckfeuclideanCubic', 'lzacityblockQuadratic', 'cjzcityblockCubic', 'jcdcityblockLinear', 'nkheuclideanFull', 'nzfcityblockFull'};
+subjectListID = {'hmn','dca', 'gfn', 'lma', 'ofv', 'sel','ckf', 'lza',  'cjz', 'jcd',  'nkh', 'nzf'};
 
 % Set directories and set which experiment to bootstrap
 % Specify basic experiment parameters
@@ -15,6 +18,7 @@ mainDir = '/Users/ana/Dropbox (Aguirre-Brainard Lab)/';
 dataDir = [mainDir 'CNST_data/ColorMaterial/E3'];
 analysisDir = [mainDir 'CNST_analysis/ColorMaterial/E3'];
 codeDir  = '/Users/ana/Documents/MATLAB/projects/Experiments/ColorMaterial/analysis';
+whichSet = 3; % 2 = colorAxis; 3 = materialAxis
 
 % sort indices.
 nRep = 10;
@@ -57,34 +61,45 @@ for s = 1:length(subjectList)
     cmMatrixNfirstChosen = nan(7,7);
     
     % Integrate all the trials and compute the probability.
-    for c = 1:nColorLevels
-        for m = 1:nMatLevels
+    for c1 = 1:nColorLevels % competitor1
+        for c2 = 1:nMatLevels % competitor 2
             
-            % Need to loop through all color/material data
+            % Need to loop through all combinations
             for i  = 1:size(newData,1)
-                firstColorMatch = (newData(i,c1Index) == 0) && (newData(i,m1Index) == (m-4));
-                secondMaterialMatch = (newData(i,c2Index) == (c-4)) && (newData(i,m2Index) == 0);
-                secondColorMatch = (newData(i,c2Index) == 0) && (newData(i,m2Index) == (m-4));
-                firstMaterialMatch = (newData(i,c1Index) == (c-4)) && (newData(i,m1Index) == 0);
-                
+                if whichSet == 1
+                    firstC1 = (newData(i,c1Index) == 0) && (newData(i,m1Index) == (c2-4));
+                    secondC2 = (newData(i,c2Index) == (c1-4)) && (newData(i,m2Index) == 0);
+                    secondC1 = (newData(i,c2Index) == 0) && (newData(i,m2Index) == (c2-4));
+                    firstC2 = (newData(i,c1Index) == (c1-4)) && (newData(i,m1Index) == 0);
+                elseif whichSet == 2
+                    firstC1 = (newData(i,c1Index) == (c1-4)) && (newData(i,m1Index) == 0);
+                    secondC2 = (newData(i,c2Index) == (c2-4)) && (newData(i,m2Index) == 0);
+                    secondC1 = (newData(i,c2Index) == (c2-4)) && (newData(i,m2Index) == 0);
+                    firstC2 = (newData(i,c1Index) == (c1-4)) && (newData(i,m1Index) == 0);
+                elseif whichSet == 3
+                    firstC1 = (newData(i,c1Index) == 0 ) && (newData(i,m1Index) == (c1-4));
+                    secondC2 = (newData(i,c2Index) == 0) && (newData(i,m2Index) == (c2-4));
+                    secondC1 = (newData(i,c2Index) == 0 ) && (newData(i,m2Index) == (c2-4));
+                    firstC2 = (newData(i,c1Index) == 0) && (newData(i,m1Index) == (c1-4));
+               end
                 % Plots the percent of time color match is chosen.
-                if firstColorMatch  && secondMaterialMatch
-                    if ~isnan(cmMatrixNfirstChosen(c,m))
+                if firstC1  && secondC2
+                    if ~isnan(cmMatrixNfirstChosen(c1,c2))
                         disp('overwriting')
-                        cmMatrixNfirstChosen(c,m) = cmMatrixNfirstChosen(c,m) + newData(i,end-1);
-                        cmMatrixNtrials(c,m) = cmMatrixNtrials(c,m) + newData(i,end);
+                        cmMatrixNfirstChosen(c1,c2) = cmMatrixNfirstChosen(c1,c2) + newData(i,end-1);
+                        cmMatrixNtrials(c1,c2) = cmMatrixNtrials(c1,c2) + newData(i,end);
                     else
-                        cmMatrixNfirstChosen(c,m) = newData(i,end-1);
-                        cmMatrixNtrials(c,m) = newData(i,end);
+                        cmMatrixNfirstChosen(c1,c2) = newData(i,end-1);
+                        cmMatrixNtrials(c1,c2) = newData(i,end);
                     end
-                elseif secondColorMatch  && firstMaterialMatch
-                    if ~isnan(cmMatrixNfirstChosen(c,m))
+                elseif secondC1  && firstC2
+                    if ~isnan(cmMatrixNfirstChosen(c1,c2))
                         disp('overwriting')
-                        cmMatrixNfirstChosen(c,m) = cmMatrixNfirstChosen(c,m) + newDataRev(i);
-                        cmMatrixNtrials(c,m) = cmMatrixNtrials(c,m) + newData(i,end);
+                        cmMatrixNfirstChosen(c1,c2) = cmMatrixNfirstChosen(c1,c2) + newDataRev(i);
+                        cmMatrixNtrials(c1,c2) = cmMatrixNtrials(c1,c2) + newData(i,end);
                     else
-                        cmMatrixNfirstChosen(c,m) = newDataRev(i);
-                        cmMatrixNtrials(c,m) = newData(i,end);
+                        cmMatrixNfirstChosen(c1,c2) = newDataRev(i);
+                        cmMatrixNtrials(c1,c2) = newData(i,end);
                     end
                 end
             end
@@ -92,5 +107,14 @@ for s = 1:length(subjectList)
     end
     cmDataPoints = cmMatrixNfirstChosen./cmMatrixNtrials;
     cd(analysisDir)
-    save([subjectListID{s} 'cmData'], 'cmDataPoints');
+    if whichSet == 1
+        save([subjectListID{s} '-colorMaterialDataPoints'], 'cmDataPoints');
+    elseif whichSet == 2
+        save([subjectListID{s} '-colorOnlyDataPoints'], 'cmDataPoints');
+    elseif whichSet == 3
+        save([subjectListID{s} '-materialOnlyDataPoints'], 'cmDataPoints');
+    end
 end
+
+
+
